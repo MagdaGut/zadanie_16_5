36e5aff3eb4134d8294f600930580045245fd84e
Magda Gutowska <m.gutowska0909@gmail.com>
2018-07-28 23:02:14 +0200

    Add files

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = (env) => ({
+    mode: env || 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                 options: {
+			        presets: ["env", "react"]
+			    }
+            },
+            {
+		        test: /\.css$/,
+		        use: [
+		            { loader: 'style-loader'},
+		            {
+		                loader: 'css-loader',
+		                options: {
+		                    modules: true
+		                }
+		            }
+		        ]
+		    }
+        ]
+    }
+});
